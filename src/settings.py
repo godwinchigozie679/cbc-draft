7ce4042daae827d626e5accc1137df5567943da3
Chigozie Godwin <godwinchigozie679@gmail.com>
2022-08-19 23:46:39 +0100

    static dir and staticfile update

--- src/settings.py
+++ src/settings.py
@@ -199,16 +199,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = BASE_DIR / "media"
 
 # whitenoise.runserver_nostatic
 
