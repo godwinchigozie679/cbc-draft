4e7eaeb46ea9fa0f277bcbf58802b53f35d59ca7
Chigozie Godwin <godwinchigozie679@gmail.com>
2022-08-21 09:08:15 +0100

    my update

--- src/settings.py
+++ src/settings.py
@@ -218,8 +218,8 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 
